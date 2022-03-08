35c1d84b514874a51c46834443c066f77e21effa
Wendell Santos <wendellsantooos@gmail.com>
2022-03-08 08:50:39 -0300

    Classes e POO

--- classes.ts
+++ classes.ts
@@ -6,7 +6,39 @@ class UserAccount {
         this.name = name;
         this.age = age;
     }
+    logDatails(): void {
+        console.log(`The player ${this.name} is ${this.age} years old.`)
+    }
+}
+
+class CharAccount extends UserAccount {
+    private nickname: string;
+    readonly level: number;
+
+    constructor(name: string, age: number, nickname: string, level: number){
+        super(name, age);
+        this.nickname = nickname;
+        this.level = level;
+    }
+
+    logCharDetails():void {
+        console.log(`The player ${this.name} has the char ${this.nickname} at level ${this.level}`)
+    }
 }
 
 const will = new UserAccount("Willian", 30);
 console.log(will);
+console.log(will.age);
+will.logDatails();
+
+const john = new CharAccount("John", 45, "johnmaster", 80);
+console.log(john);
+john.logDatails();
+john.logCharDetails();
+console.log(john.level);
+
+const wendell = new CharAccount("Wendell", 28, "Farrapito", 100);
+console.log(wendell);
+wendell.logDatails();
+wendell.logCharDetails();
+console.log(wendell.level);
--- dist/classes.js
+++ dist/classes.js
@@ -4,6 +4,31 @@ class UserAccount {
         this.name = name;
         this.age = age;
     }
+    logDatails() {
+        console.log(`The player ${this.name} is ${this.age} years old.`);
+    }
+}
+class CharAccount extends UserAccount {
+    constructor(name, age, nickname, level) {
+        super(name, age);
+        this.nickname = nickname;
+        this.level = level;
+    }
+    logCharDetails() {
+        console.log(`The player ${this.name} has the char ${this.nickname} at level ${this.level}`);
+    }
 }
 const will = new UserAccount("Willian", 30);
 console.log(will);
+console.log(will.age);
+will.logDatails();
+const john = new CharAccount("John", 45, "johnmaster", 80);
+console.log(john);
+john.logDatails();
+john.logCharDetails();
+console.log(john.level);
+const wendell = new CharAccount("Wendell", 28, "Farrapito", 100);
+console.log(wendell);
+wendell.logDatails();
+wendell.logCharDetails();
+console.log(wendell.level);
